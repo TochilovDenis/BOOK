7f866d208be5927386e30c5448f5b1e7fae42489
Denis <120037570+TochilovDenis@users.noreply.github.com>
2024-01-10 23:35:26 +0300

    Создан две разные книги. Вывести их значения на экран.

--- Book/Source.cpp
+++ Book/Source.cpp
@@ -12,6 +12,25 @@ struct Book
 };
 
 int main() {
+	setlocale(LC_ALL, "");
+
+	Book book1, book2{ "C++", 1000, 2005, Leather };
+
+	book1.title = "The Great Book";
+	book1.amountOfPages = 500;
+	book1.yearOfPublication = 2019;
+	book1.Binding = Leather;
+
+	cout << "Название: " << book1.title << "\n";
+	cout << "Количество страниц: " << book1.amountOfPages << "\n";
+	cout << "Год издания: " << book1.yearOfPublication << "\n";
+	cout << "Переплёт: " << (book1.Binding == Leather ? "Кожа" : book1.Binding == Paper ? "Бумага" : "Картон") << "\n";
+	cout << endl;
+
+	cout << "Название: " << book2.title << "\n";
+	cout << "Количество страниц: " << book2.amountOfPages << "\n";
+	cout << "Год издания: " << book2.yearOfPublication << "\n";
+	cout << "Переплёт: " << (book2.Binding == Leather ? "Кожа" : book2.Binding == Paper ? "Бумага" : "Картон") << "\n";
 	
 	return 0;
 }
