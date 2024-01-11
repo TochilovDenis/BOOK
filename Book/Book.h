05ab7cb445fc9630e8126e98923968fb940461e4
Denis <120037570+TochilovDenis@users.noreply.github.com>
2024-01-12 00:44:44 +0300

    Код перенесен:  - Book.h  - Data.h

---
+++ Book/Book.h
@@ -0,0 +1,54 @@
+#ifndef BOOK_H
+#define BOOK_H
+
+#include <cstdlib>
+#include <ctime>
+#include <iostream>
+#include <string>
+
+#include "Data.h"
+
+using namespace std;
+
+enum typeOfBinding {
+	Leather,
+	Paper,
+	Cardboard,
+
+	bindingsTotal
+};
+
+string typeOfBindingStr[bindingsTotal] = { "Кожа", "Бумага", "Картон" };
+
+struct Book
+{
+	string title = "";
+	string author = "";
+	int amountOfPages = 0;
+	int yearOfPublication = 0;
+	typeOfBinding binding{};
+
+	string getBookStatus() {
+		return  title + " / " + author + " : " + to_string(yearOfPublication) + " год, " + to_string(amountOfPages) + "с. | Переплет: " + typeOfBindingStr[binding];
+	}
+};
+
+void fillArrayBooks(Book* books, int numBooks) {
+	srand(static_cast<unsigned>(time(0))); srand(static_cast<unsigned>(time(0)));
+	for (size_t i = 0; i < numBooks; i++) {
+		books[i].title = BooksTitles[i];
+		books[i].author = BooksAuthors[i];
+		books[i].amountOfPages = rand() % 1000 + 1;
+		books[i].yearOfPublication = rand() % 223 + 1800;
+		books[i].binding = typeOfBinding(rand() % bindingsTotal);
+	}
+}
+
+void printArrayBooks(Book* books, int numBooks) {
+	for (size_t i = 0; i < numBooks; i++)
+	{
+		cout << "[" << i + 1 << "] " << books[i].getBookStatus() << endl;
+	}
+}
+
+#endif /*BOOK_H*/
--- Book/Source.cpp
+++ Book/Source.cpp
@@ -1,66 +1,13 @@
-﻿#include <iostream>
-#include <string>
-using namespace std;
-
-enum typeOfBinding { 
-	Leather, 
-	Paper, 
-	Cardboard, 
-	
-	bindingsTotal };
-
-string typeOfBindingStr[bindingsTotal] = { "Кожа", "Бумага", "Картон" };
-
-string BooksTitles[10] = { "Собор Парижской Богоматери", "Дневник Анны Франк",
-						 "Грозовой перевал", "Сто лет одиночества", "Великий Гэтсби",
-						 "Приключения Шерлока Холмса", "Мастер и Маргарита",
-						 "Атлант расправил плечи", "Три товарища", "Робинзон Крузо" };
-
-string BooksAuthors[10] = { "Виктор Гюго", "Анна Франк", "Эмили Бронте", "Габриэль Гарсия Маркес",
-						   "Фрэнсис Скотт Фицджеральд", "Артур Конан Дойл", "Михаил Булгаков",
-						   "Айн Рэнд", "Эрих Мария Ремарк", "Даниель Дефо" };
-
-struct Book
-{
-	string title = "";
-	string author = "";
-	int amountOfPages = 0;
-	int yearOfPublication = 0;
-	typeOfBinding binding{};
-
-	string getBookStatus()  {
-		return  title +	" / " + author + " : " +  to_string(yearOfPublication) + " год, " +to_string(amountOfPages) + "с. | Переплет: " + typeOfBindingStr[binding];
-	}
-};
-
-void fillArrayBooks(Book*, int); 
-void printArrayBooks(Book* , int);
+﻿#include "Book.h"
 
 int main() {
 	setlocale(LC_ALL, "");
-	srand(time(0));
-	const unsigned int numBooks = 10;
+	
+	unsigned int numBooks = 10;
 	Book* books = new Book[numBooks];
 	fillArrayBooks(books, numBooks);
 	printArrayBooks(books, numBooks);
 	delete[] books;
-
 	return 0;
 }
 
-void fillArrayBooks(Book* books, int numBooks) {
-	for (size_t i = 0; i < numBooks; i++ ) {
-		books[i].title = BooksTitles[i];
-		books[i].author = BooksAuthors[i];
-		books[i].amountOfPages = rand() % 1000 + 1;
-		books[i].yearOfPublication = rand() % 223 + 1800;
-		books[i].binding = typeOfBinding(rand() % bindingsTotal);
-	}
-}
-
-void printArrayBooks(Book* books, int numBooks) {
-	for (size_t i = 0; i < numBooks; i++)
-	{
-		cout << "[" << i + 1 << "]" << books[i].getBookStatus() << endl;
-	}
-}
